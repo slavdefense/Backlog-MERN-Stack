ec50235b3dd2a3953fd5a83d15d1cae0a17d16c4
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-02 06:47:42 -0800

    Define tickets controller and connect ticket model and router. Basics only

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import * as ticketCtrl from '../controllers/tickets.js'
 
 
 const router = Router()
 
 /*---------- Public Routes ----------*/
+
+router.get('/',ticketCtrl.index)
 
 
 /*---------- Protected Routes ----------*/
