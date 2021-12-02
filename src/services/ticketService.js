98ce9a1a38acae7460a5204bc27a01e32cb9ac5d
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-02 07:35:05 -0800

    Define CRUD calls on TicketServices

---
+++ src/services/ticketService.js
@@ -0,0 +1,45 @@
+import * as tokenService from '../services/tokenService'
+const BASE_URL = '/api/tickets'
+
+function createTicket(ticketData) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`, 'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(ticketData)
+  })
+  .then(res => res.json())
+}
+
+function getTickets() {
+  return fetch(BASE_URL, {
+    headers: {'Authorization': `Bearer ${tokenService.getToken()}`}
+  })
+  .then(res => res.json())
+}
+function deleteTicket(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {'Authorization': `Bearer ${tokenService.getToken()}`}
+  })
+  .then(res => res.json())
+}
+
+function updateTicket(ticketData) {
+  return fetch(`${BASE_URL}/${ticketData.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`, 'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(ticketData)
+  })
+  .then(res => res.json())
+}
+
+export{
+  createTicket,
+  getTickets,
+  deleteTicket,
+  updateTicket
+}
