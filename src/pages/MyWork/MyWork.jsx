23a6b226019ce1af9f36dec97de44131add52215
Meiling Bedard <meiling.bedard@gmail.com>
2021-12-03 14:54:08 -0500

    fix direct navigation to myWork page

--- src/pages/MyWork/MyWork.jsx
+++ src/pages/MyWork/MyWork.jsx
@@ -1,5 +1,5 @@
 import './MyWork.css'
-import React, { useState } from 'react'
+import React from 'react'
 
 const MyWork = props => {
   const profile = props.profile
@@ -13,25 +13,27 @@ const MyWork = props => {
       <>
         <div className="myWork">
           <h1>Hello, {props.user.name}</h1>
-          <h3>Your tickets</h3>
           {profile.profile && profile.profile.tickets.map((ticket) => {
             return (
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{ticket.title}</td>
-                    <td>{ticket.description}</td>
-                    <td>{ticket.status}</td>
-                  </tr>
-                </tbody>
-              </table>
+              <>
+                <h3>Your tickets</h3>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{ticket.title}</td>
+                      <td>{ticket.description}</td>
+                      <td>{ticket.status}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </>
             )
           })
           }
