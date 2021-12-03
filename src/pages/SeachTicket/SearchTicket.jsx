a568dcfa6fd8516ab106ee14c355189432c17877
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-03 11:58:56 -0800

    create input

--- src/pages/SeachTicket/SearchTicket.jsx
+++ src/pages/SeachTicket/SearchTicket.jsx
@@ -2,6 +2,34 @@ const SearchTicket = () => {
   return ( 
     <div className="container">
       <h1>I am the search page</h1>
+      <form action="">
+        <input type="text" />
+        <input type="date" name="" id="" />
+        <select
+          name="priority"
+         
+        >
+          <option
+            name="priority"
+            value='High'
+          >
+            High
+          </option>
+          <option 
+            name="priority"
+            value='Medium'
+          >
+            Medium
+          </option>
+          <option
+            name="priority" 
+            value='Low'
+          >
+            Low
+          </option>
+        </select>
+
+      </form>
 
     </div>
    );
