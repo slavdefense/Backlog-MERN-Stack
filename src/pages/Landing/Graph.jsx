266385c8627d3e19cf01c4e07a1b2f006c125fda
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-04 18:50:09 -0800

    Display a bar graph for Tickets based on priority of the total percentages of tickets

--- src/pages/Landing/Graph.jsx
+++ src/pages/Landing/Graph.jsx
@@ -2,29 +2,64 @@ import React from 'react';
 import "./Graph.css"
 const Graph = ({graphAllTickets}) => {
 
+  const maxValue = graphAllTickets.length
+  let totalLow = 0
+  let totalMedium = 0
+  let totalHigh = 0
 
+  for(let item of graphAllTickets){
+    if(item.priority==='Low'){
+      totalLow++
+    }
+    else if(item.priority==="Medium"){
+      totalMedium++
+    }
+    else if(item.priority==="High")
+    totalHigh++
 
+  }
+
+  let totalLowPercentage = Math.floor((totalLow/maxValue)*100)
+  let totalMediumPercentage = Math.floor((totalMedium/maxValue)*100)
+  let totalHighPercentage = Math.floor((totalHigh/maxValue)*100)
+  
+  // console.log(total)
+console.log(totalLowPercentage)
+console.log(totalMediumPercentage)
+console.log(totalHighPercentage)
 
   return ( 
     <div className="graphs">
+
+    
+
+
+
     <div className="priority-bar">
-      <label htmlFor="low">Low</label>
-      <div id="low" className="priority-bar-fill" style={{'height':'80%'}}>
+      <label className="sp" htmlFor="low">Low {totalLow}</label>
+      <div id="low" className="priority-bar-fill" style={{'height':`${totalLowPercentage}%`}}>
+      </div>
+      
+    </div>
+    <div className="priority-bar">
+      <label htmlFor="medium">Medium {totalMedium}</label>
+      <div id="medium" className="priority-bar-fill" style={{'height':`${totalMediumPercentage}%`}}>
       </div>
 
     </div>
     <div className="priority-bar">
-      <label htmlFor="medium">Medium</label>
-      <div id="medium" className="priority-bar-fill" style={{'height':'80%'}}>
+      <label className="sp" htmlFor="high">High {totalHigh}</label>
+      <div id="high" className="priority-bar-fill" style={{'height':`${totalHighPercentage}%`}}>
       </div>
+      
 
     </div>
-    <div className="priority-bar">
-      <label htmlFor="high">High</label>
-      <div id="high" className="priority-bar-fill" style={{'height':'80%'}}>
-      </div>
+   
+    <div className="circle">
+
 
     </div>
+
    
     </div>
     
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,23 +5,28 @@ import Graph from './Graph';
 
 const Landing = ({user,allTickets}) => {
 
-  const [graphAllTickets,setgraphAllTickets] =useState()
+  
+
+  const [graphAllTickets,setgraphAllTickets] =useState(allTickets)
 
   useEffect(()=>{
     setgraphAllTickets(allTickets)
   },[])
 
+ 
 
-  console.log(graphAllTickets)
+  
   return (
     <main className={styles.container}>
       <h1>
-        Ke paso amigo, {user ? user.name : "Friend"} Help us Solve some Erors
+        Hi, {user ? user.name : "Friend"} Lets Track  Errors.We Love you!
       </h1>
-      <h1>3 Graphs</h1>
+      
+      {/* {console.log(graphAllTickets)} */}
 
       
-    <Graph graphAllTickets={graphAllTickets}/>
+    {/* <Graph graphAllTickets={graphAllTickets}/> */}
+    <Graph graphAllTickets={allTickets}/>
      
 
     </main>
