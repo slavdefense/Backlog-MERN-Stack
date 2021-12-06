de5ca27a77626ea14fd614d021784ab97858da87
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-06 01:08:49 -0800

    Tracking daily bugs

--- src/pages/Landing/Graph.jsx
+++ src/pages/Landing/Graph.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import "./Graph.css"
+
+
 const Graph = ({graphAllTickets}) => {
+  
+
+// const [ticketDaysValue,setTicketDaysValue] = useState()
+
+// useEffect(()=>{
+//   setTicketDaysValue(setTicketDaysValue)
+// },[graphAllTickets])
+// console.log(graphAllTickets[0].createdAt)
 
   const maxValue = graphAllTickets.length
   let totalLow = 0
@@ -28,40 +38,111 @@ console.log(totalLowPercentage)
 console.log(totalMediumPercentage)
 console.log(totalHighPercentage)
 
+
+
+const days = [
+  {day:'Sunday',value:0},
+  {day:'Monday',value:0},
+  {day:'Tuesday',value:0},
+  {day:'Wednesday',value:0},
+  {day:'Thursday',value:0},
+  {day:'Friday',value:0},
+  {day:'Saturday',value:0},
+  
+]
+// console.log(graphAllTickets[0].createdAt)
+if(graphAllTickets){
+  for(let i=0;i<graphAllTickets.length;i++){
+    if(new Date(graphAllTickets[i].createdAt.slice(0,10)).getDay()===0){
+      days[0].value++
+    }
+   
+  }
+  for(let item of days){
+    console.log(item.value)
+  }
+
+
+
+}
+
+
+
+
+
   return ( 
-    <div className="graphs">
+    <section>
+      <div className="graphs">
 
+<div className="priority-bar">
+  <label className="sp" htmlFor="low">Low {totalLow}</label>
+  <div id="low" className="priority-bar-fill" style={{'height':`${totalLowPercentage}%`}}>
+  </div>
+  
+</div>
+<div className="priority-bar">
+  <label htmlFor="medium">Medium {totalMedium}</label>
+  <div id="medium" className="priority-bar-fill" style={{'height':`${totalMediumPercentage}%`}}>
+  </div>
+
+</div>
+<div className="priority-bar">
+  <label className="sp" htmlFor="high">High {totalHigh}</label>
+  <div id="high" className="priority-bar-fill" style={{'height':`${totalHighPercentage}%`}}>
+  </div>
+  
+
+</div>
+
+<div className="circle">
+
+
+</div>
+
+
+
+</div>
+
+
+
+<div className="days-of-the-week">
+  <div className="days">
+  <label className="sp" htmlFor="low">Sunday </label>
+    <div className="days-inner"style={{'height':`${days[0].value}%`}} ></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Monday </label>
+    <div className="days-inner"style={{'height':`${days[1].value}%`}}></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Tuesday </label>
+    <div className="days-inner"style={{'height':`${days[2].value}%`}}></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Wednesday </label>
+    <div className="days-inner"style={{'height':`${days[3].value}%`}}></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Thursday </label>
+    <div className="days-inner"style={{'height':`${days[4].value}%`}}></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Friday </label>
+    <div className="days-inner"style={{'height':`${days[5].value}%`}}></div>
+  </div>
+  <div className="days">
+  <label className="sp" htmlFor="low">Saturday </label>
+    <div className="days-inner"style={{'height':`${days[6].value}%`}}></div>
+  </div>
+  
+  
+
+</div>
+
+
+    </section>
     
-
-
-
-    <div className="priority-bar">
-      <label className="sp" htmlFor="low">Low {totalLow}</label>
-      <div id="low" className="priority-bar-fill" style={{'height':`${totalLowPercentage}%`}}>
-      </div>
-      
-    </div>
-    <div className="priority-bar">
-      <label htmlFor="medium">Medium {totalMedium}</label>
-      <div id="medium" className="priority-bar-fill" style={{'height':`${totalMediumPercentage}%`}}>
-      </div>
-
-    </div>
-    <div className="priority-bar">
-      <label className="sp" htmlFor="high">High {totalHigh}</label>
-      <div id="high" className="priority-bar-fill" style={{'height':`${totalHighPercentage}%`}}>
-      </div>
-      
-
-    </div>
-   
-    <div className="circle">
-
-
-    </div>
-
-   
-    </div>
+    
     
    );
 }
