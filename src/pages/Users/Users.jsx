429a26e4ff14dabbe9fdbfe6c33dbc562405b090
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-07 13:14:25 -0800

    selects

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -4,27 +4,108 @@ import './User.css'
 import { Link } from 'react-router-dom'
 
 const Users = (props) => {
+  console.log(props.user)
   const [profiles, setProfiles] = useState([])
+  // console.log(profiles)
 
   useEffect(()=> {
     profileService.getAllProfiles()
     .then(profiles => setProfiles(profiles))
   }, [])
 
+  const handleClick =(evt)=>{
+    evt.preventDefault()
+    
+    // console.log(evt)
+
+  }
+
   return (
     <div className="user-list container">
-      <h1>Hello.  This is a list of all the users.</h1>
+      <h1>Greetings {props.user.name}  {props.user.email} </h1>
+      <h4>You are  {!props.user.isAdmin? 'Not an admin':'Is an Admin'} </h4>
+
+      <h6>Assign users to a team</h6>
+  
       {profiles.length ? 
-      <>
-        {profiles.map(profile=>
-          <Link to="/viewUser" state={profile}>
-          <p key={profile._id}>{profile.name}</p>
+      <div id="cont-d">
+        
+        {profiles.map((profile)=> {
+          return(
+            <div className="assign">
+              <Link to="/viewUser" state={profile}>
+            
+            
+            <li className="vertical-scroll" key={profile._id}>{profile.name}</li> 
+ 
+            
+            
           </Link>
+          <form action=""  >
+         
+          <button onClick={handleClick} >
+          <li  className="assign-click" >Assign</li>
+
+
+          </button>
+
+          </form>
+          
+            </div>
+            
+          )
+           
+        }         
         )}
-      </>
+        </div>
+        
       :
         <p>An error occured</p>
       }
+
+
+      <div>
+        <h4>Select a team to Assign</h4>
+        <select 
+          name="team"
+         
+        >
+           <option
+            
+            value='none'
+          >
+            Select an option
+          </option>
+         
+          <option
+            name="frontend"
+            value='frontend'
+          >
+            High-Prority
+          </option>
+          <option 
+            name="backend"
+            value='Medium'
+          >
+          Medium-Priority
+          </option>
+          <option
+            name="fullstack" 
+            value='Low'
+          >
+            Low-Priority
+          </option>
+        </select>
+
+
+      </div>
+
+      <div >
+<h1>List of users assigned profile</h1>
+
+
+      </div>
+      
     </div>
   );
 }
